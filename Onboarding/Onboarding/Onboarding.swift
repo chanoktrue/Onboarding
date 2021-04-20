0c8825b1e9747e3d10b246c3d23d3a6787292247
Thongchai Subsaidee <chanoktrue@me.com>
2021-04-20 15:47:47 +0700

    first project

---
+++ Onboarding/Onboarding/Onboarding.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding.swift
+//  Onboarding
+//
+//  Created by Thongchai Subsaidee on 20/4/2564 BE.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding()
+    }
+}
---
+++ Onboarding/Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Thongchai Subsaidee on 20/4/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
