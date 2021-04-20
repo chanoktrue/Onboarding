c39f3427942c29e478cbe5c530e6b3b2f7085615
Thongchai Subsaidee <chanoktrue@me.com>
2021-04-20 17:47:17 +0700

    Create OnboardingCardView

--- Onboarding/Onboarding/ContentView.swift
+++ Onboarding/Onboarding/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            
+            Text("Hello, world!")
+                .padding()
+                .opacity(0)
+            
+ 
+            
+        }
+        
     }
 }
 
--- Onboarding/Onboarding/Onboarding.swift
+++ Onboarding/Onboarding/Onboarding.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 struct Onboarding: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            TabView {
+                OnboardingCardView()
+                OnboardingCardView()
+                OnboardingCardView()
+            }
+            .tabViewStyle(PageTabViewStyle())
+        }
     }
 }
 
 struct Onboarding_Previews: PreviewProvider {
     static var previews: some View {
         Onboarding()
+    }
+}
+
+struct OnboardingCardView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("120 hours of video")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text("Learn design and code")
+                .font(.largeTitle)
+                .bold()
+            Text("Learn by building real apps using Swift, React, Flutter and more. Deign files and sorce code an icluded for each corse.")
+                .font(.subheadline)
+        }
+        .padding(30)
+        .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)), Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))]), startPoint: .top, endPoint: .bottom))
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .padding(20)
     }
 }
